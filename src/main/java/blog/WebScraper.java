e3e51a801d501348b0ea6aa924518016751f06a2
ksahin <sahin.kevin@gmail.com>
2016-02-07 23:09:42 +0100

    add JSON export

--- src/main/java/blog/WebScraper.java
+++ src/main/java/blog/WebScraper.java
@@ -1,8 +1,10 @@
 package blog;
 
+import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
@@ -13,27 +15,35 @@ public class WebScraper {
 	public static void main(String[] args) {
 		
 	    String searchQuery = "iphone 6s" ;
-		
+		String baseUrl = "https://newyork.craigslist.org/" ;
 		WebClient client = new WebClient();
 		client.setJavaScriptEnabled(false);
 		try {
-			String searchUrl = "https://newyork.craigslist.org/search/sss?sort=rel&query=" + URLEncoder.encode(searchQuery, "UTF-8");
+			String searchUrl = baseUrl + "search/sss?sort=rel&query=" + URLEncoder.encode(searchQuery, "UTF-8");
 			HtmlPage page = client.getPage(searchUrl);
 			
 			List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//span[@class='rows']/p") ;
 			if(items.isEmpty()){
 				System.out.println("No items found !");
 			}else{
-				for(HtmlElement item : items){
-					HtmlAnchor itemAnchor = ((HtmlAnchor) item.getFirstByXPath(".//span[@class='txt']/span[@class='pl']/a"));
+				for(HtmlElement htmlItem : items){
+					HtmlAnchor itemAnchor = ((HtmlAnchor) htmlItem.getFirstByXPath(".//span[@class='txt']/span[@class='pl']/a"));
+					HtmlElement spanPrice = ((HtmlElement) htmlItem.getFirstByXPath(".//span[@class='txt']/span[@class='l2']/span[@class='price']")) ;
 					
-					String itemName = itemAnchor.asText();
-					String itemUrl = itemAnchor.getHrefAttribute() ;
+					// It is possible that an item doesn't have any price, we set the price to 0.0 in this case
+					String itemPrice = spanPrice == null ? "0.0" : spanPrice.asText() ;
 					
-					HtmlElement spanPrice = ((HtmlElement) item.getFirstByXPath(".//span[@class='txt']/span[@class='l2']/span[@class='price']")) ;
-					// It is possible that an item doesn't have any price
-					String itemPrice = spanPrice == null ? "no price" : spanPrice.asText() ;
-					System.out.println( String.format("Name : %s Url : %s Price : %s", itemName, itemPrice, itemUrl));
+					Item item = new Item();
+					item.setTitle(itemAnchor.asText());
+					item.setUrl( baseUrl + itemAnchor.getHrefAttribute());
+					
+					item.setPrice(new BigDecimal(itemPrice.replace("$", "")));
+				
+					
+					ObjectMapper mapper = new ObjectMapper();
+					String jsonString = mapper.writeValueAsString(item) ;
+					
+					System.out.println(jsonString);
 				}
 			}
 		} catch(Exception e){
